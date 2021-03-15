57cbb8492c9f76bb5e074dbdcd6463d1bd405f72
MatveyMA <matveym2048@gmail.com>
2021-03-15 04:41:13 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort (matrix) 
+{
+  let c = [];
+if((typeof matrix)!== "undefined"){ 
+  let x = matrix.length;
+for(i=0;i<x;i++)
+{
+  if(i%2==0)
+  {
+      odd = matrix[i];
+      c.push(odd);
+  }
+  else if(i%2!==0)
+  {
+      even = matrix[i].reverse();
+      c.push(even);
+  }
+    if((typeof odd) !== "undefined" && (typeof even) !== "undefined" && (typeof c) == "undefined")
+    {
+    }
+}
+} 
+else{
   return [];
 }
+b= c.flat(Infinity);
+  return b;
+}
+
